14b6f9b3696ad5b4a10fef0e83fe4e0ab08da978
ccomapjw <ccomapjw@naver.com>
2020-04-05 14:19:36 +0900

    FireChat2

--- app/src/main/java/net/skhu/firechat2/VideoPreview.java
+++ app/src/main/java/net/skhu/firechat2/VideoPreview.java
@@ -1,8 +1,11 @@
 package net.skhu.firechat2;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.database.Cursor;
@@ -15,7 +18,9 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.MediaController;
@@ -23,6 +28,8 @@ import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.io.File;
+
+import static androidx.core.content.FileProvider.getUriForFile;
 
 public class VideoPreview extends AppCompatActivity {
 
@@ -34,16 +41,26 @@ public class VideoPreview extends AppCompatActivity {
 
     boolean horizontalMode;
 
+    Menu mMenu;
+
+    ActionBar actionBar;
+    boolean actionShow;
+
+    File videoFile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_preview);
+
 
         currentPos=0;
 
         horizontalMode = false;
 
         videoViewPreview = findViewById(R.id.videoViewPreview);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -52,86 +69,26 @@ public class VideoPreview extends AppCompatActivity {
         }
 
         File path = getFilesDir();
-        File videoFile = new File(path, videoFileName);
+        videoFile = new File(path, videoFileName);
 
         if (videoFile.exists()) {
             videoViewPreview.setVideoPath(videoFile.toString());
         }
 
-        /*Intent intent = new Intent(Intent.ACTION_VIEW);
-
-        Uri uri = Uri.parse(videoFile.toString());
-
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = FileProvider.getUriForFile(getApplicationContext(), BuildConfig.APPLICATION_ID, videoFile);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
         intent.setDataAndType(uri, "video/*");
+        startActivity(intent);
 
-        if (intent != null && intent.resolveActivity(getPackageManager()) != null) {
-
-            startActivity(intent);
-
-        }*/
 
 
         //미디어컨트롤러 추가하는 부분
-        MediaController controller = new MediaController(this);
-        videoViewPreview.setMediaController(controller);
+        //MediaController controller = new MediaController(this);
+        //videoViewPreview.setMediaController(controller);
 
         //비디오뷰 포커스를 요청함
         videoViewPreview.requestFocus();
-
-
-        /*//동영상이 재생준비가 완료되었을 때를 알 수 있는 리스너 (실제 웹에서 영상을 다운받아 출력할 때 많이 사용됨)
-        videoViewPreview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                //Toast.makeText(getApplicationContext(), "동영상이 준비되었습니다. \n'시작' 버튼을 누르세요", Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(), "동영상이 준비되었습니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        //동영상 재생이 완료된 걸 알 수 있는 리스너
-        videoViewPreview.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                //동영상 재생이 완료된 후 호출되는 메소드
-                //Toast.makeText(getApplicationContext(), "동영상 재생이 완료되었습니다.", Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(), "동영상 재생이 완료되었습니다.", Toast.LENGTH_SHORT).show();
-            }
-        });*/
-
-
-        /*Button b = (Button) findViewById(R.id.btnExitVideo);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                finish();
-            }
-        });
-
-        b = (Button) findViewById(R.id.btnStartInPreview);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                playVideo();
-            }
-        });
-
-        b = (Button) findViewById(R.id.btnStopInPreview);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                stopVideo();
-            }
-        });
-
-        b = (Button) findViewById(R.id.btnVideoEdit);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                String key = ((MainActivity) MainActivity.mContext).itemList.getKey(selectIndex);
-                ((MainActivity) MainActivity.mContext).firebaseDbService.removeFromServer(key);
-                finish();
-            }
-        });*/
 
         videoViewPreview.setOnErrorListener(new MediaPlayer.OnErrorListener() {
             @Override
@@ -146,8 +103,9 @@ public class VideoPreview extends AppCompatActivity {
         videoViewPreview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
-                Toast.makeText(getApplicationContext(), "동영상이 준비되었습니다. \n'시작' 버튼을 누르세요", Toast.LENGTH_SHORT).show();
                 videoViewPreview.seekTo(currentPos);
+                videoViewPreview.requestFocus();
+                Toast.makeText(getApplicationContext(), "동영상이 준비되었습니다. \n'시작' 버튼을 누르세요", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -159,7 +117,20 @@ public class VideoPreview extends AppCompatActivity {
             }
         });
 
-        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //가로모드 고정
+        videoViewPreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = FileProvider.getUriForFile(getApplicationContext(), BuildConfig.APPLICATION_ID, videoFile);
+                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                intent.setDataAndType(uri, "video/*");
+                startActivity(intent);
+            }
+        });
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //가로모드 고정
+        horizontalMode = true;
+        invalidateOptionsMenu();
     }
 
     //동영상 재생 Method
@@ -179,29 +150,6 @@ public class VideoPreview extends AppCompatActivity {
 //        videoView = null;
     }
 
-    public Uri getUriFromPath(String path) {
-
-        String fileName = path;
-
-        Uri fileUri = Uri.parse(fileName);
-
-        String filePath = fileUri.getPath();
-
-        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-
-                null, "_data = '" + filePath + "'", null, null);
-
-        cursor.moveToNext();
-
-        int id = cursor.getInt(cursor.getColumnIndex("_id"));
-
-        Uri uri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-
-        return uri;
-
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_video_preview, menu);
@@ -210,6 +158,8 @@ public class VideoPreview extends AppCompatActivity {
         MenuItem menuItem_verticalMode = menu.findItem(R.id.action_verticalMode); // 화면 세로 메뉴
         menuItem_horizontalMode.setVisible(!horizontalMode);// 화면 가로 상태라면 화면 세로 메뉴가 보임
         menuItem_verticalMode.setVisible(horizontalMode);// 화면 가로 상태이면 화면 세로 메뉴가 보임
+
+        mMenu = menu;
 
         return true;
     }
@@ -243,6 +193,11 @@ public class VideoPreview extends AppCompatActivity {
             horizontalMode = false;
             invalidateOptionsMenu(); // 메뉴를 상태를 변경해야 함
         }
+        else if (id == R.id.action_fullScreen){
+            actionBar = getSupportActionBar();
+            actionBar.hide();
+            actionShow = false;
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -251,6 +206,7 @@ public class VideoPreview extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
     }
+
 
     @Override
     protected void onPause() {
@@ -262,5 +218,20 @@ public class VideoPreview extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         videoViewPreview.seekTo(currentPos);
+        videoViewPreview.requestFocus();
+    }
+
+    private Uri getUriFromPath(String filePath) {
+        long photoId;
+        Uri photoUri = MediaStore.Video.Media.getContentUri("external");
+        String[] projection = {MediaStore.Video.VideoColumns._ID};
+        Cursor cursor = getContentResolver().query(photoUri, projection, MediaStore.Video.VideoColumns.DATA + " LIKE ?", new String[] { filePath }, null);
+        cursor.moveToFirst();
+
+        int columnIndex = cursor.getColumnIndex(projection[0]);
+        photoId = cursor.getLong(columnIndex);
+
+        cursor.close();
+        return Uri.parse(photoUri.toString() + "/" + photoId);
     }
 }
